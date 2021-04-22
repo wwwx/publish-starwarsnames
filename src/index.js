d4932b0f7ecc15967777595e274c0a8a6996de7d
wangxu <risin_wangx@163.com>
2021-04-23 06:45:48 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const starWarsNames = require('./starwars-names.json')
+const uniqueRandomArray = require('unique-random-array')
+
+module.exports = {
+    all: starWarsNames,
+    random: uniqueRandomArray(starWarsNames)
+}
